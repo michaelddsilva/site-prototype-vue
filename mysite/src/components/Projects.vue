15b5be51d0340307c8e76392d53505cd91a1a2b6
michaelddsilva <michael.dsilva.343@gmail.com>
2020-03-08 19:49:03 -0400

    updated projects section

--- mysite/src/components/Projects.vue
+++ mysite/src/components/Projects.vue
@@ -4,17 +4,20 @@
     <div class="div2">
         <h3>Personal Website: <a href="javascript:alert('You are already here!');">michaeldsilva.com</a> </h3>
         <img src="../assets/images/project_images/m.png" height="60px">
+        <img src="../assets/images/project_images/vue.png" height="60px">
         <ul>
             <li>This site is hosted through <a href="https://github.io" target ="_blank">github.io</a>.</li>
             <li>The repository for the site can be located <a href="https://github.com/michaelddsilva/michaelddsilva.github.io" target ="_blank">here</a>.</li>
             <li>The icons i'm using in site sections are from <a target="_blank" href="https://icons8.com">Icons8</a>.</li>
             <li>I started this site in Febuary of 2019 and have been updating it with my recent experiences, and tweaking it as I improve my experience as a web developer.</li>
+            <li>This site implements <a href="https://vuejs.org/">Vue.js</a>.</li>
         </ul>
     </div>
 
     <div class="div2">
     <h3>Shoot 'em up Genre Game: <a href="https://michaelddsilva.github.io/assets/game/" target ="_blank">[play here]</a></h3>
     <img src="../assets/images/project_images/spaceinvaders.png" height="60px">
+    <img src="../assets/images/project_images/c_sharp.png" height="60px">
     <ul>
         <li>This "shoot 'em up game" was created in collaboration with two other software engineering students. </li> 
         <li>The game's scripts were coded in C# and the assets and scenes were created in <a href="https://unity.com/" target="_blank">Unity</a>. </li>
@@ -41,8 +44,11 @@
 
     <div class="div2">
     <h3>Mobile Android Application</h3>
+    <img src="../assets/images/project_images/android.png" height="60px">
+    <img src="../assets/images/project_images/java.png" height="60px">
     <ul>
         <li>This application is being created for a Software Engineering Design course. </li>
+        <li>The application was created in Android Studio. </li>
     </ul>
     </div>
   </div>

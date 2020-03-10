276c7e7c775343592cb1ee9caa91bbd4b8e599f4
michaelddsilva <michael.dsilva.343@gmail.com>
2020-03-10 01:00:00 -0400

    adapted projects section to use JSON
    edited projects content

--- mysite/src/components/Projects.vue
+++ mysite/src/components/Projects.vue
@@ -1,62 +1,29 @@
 <template>
   <div class="container">
     <h1>Projects</h1>
-    <div class="div2">
-        <h3>Personal Website: <a href="javascript:alert('You are already here!');">michaeldsilva.com</a> </h3>
-        <img src="img/project_images/m.png" height="60px">
-        <img src="img/project_images/vue.png" height="60px">
-        <ul>
-            <li>This site is hosted through <a href="https://github.io" target ="_blank">github.io</a>.</li>
-            <li>The repository for the site can be located <a href="https://github.com/michaelddsilva/michaelddsilva.github.io" target ="_blank">here</a>.</li>
-            <li>The icons i'm using in site sections are from <a target="_blank" href="https://icons8.com">Icons8</a>.</li>
-            <li>I started this site in Febuary of 2019 and have been updating it with my recent experiences, and tweaking it as I improve my experience as a web developer.</li>
-            <li>This site implements <a href="https://vuejs.org/">Vue.js</a>.</li>
+    <div class="div2" v-for="data in projectsJson" v-bind:key="data">
+        <h3>{{data.title}}</h3>
+        <p v-if="data.link">Link: <a :href="data.link">{{data.linkDisplay}}</a></p>
+        <p v-if="data.repo">Code: <a :href="data.code">{{data.repo}}</a></p>
+        <i v-for="img in data.img" v-bind:key="img">
+            <img :src="img" height="60px">
+        </i>
+        <ul v-for="des in data.description" v-bind:key="des">
+            <li v-html="des"></li>
         </ul>
-    </div>
-
-    <div class="div2">
-    <h3>Shoot 'em up Genre Game: <a href="https://michaelddsilva.github.io/game/" target ="_blank">[play_here]</a></h3>
-    <img src="img/project_images/spaceinvaders.png" height="60px">
-    <img src="img/project_images/c_sharp.png" height="60px">
-    <ul>
-        <li>This "shoot 'em up game" was created in collaboration with two other software engineering students. </li> 
-        <li>The game's scripts were coded in C# and the assets and scenes were created in <a href="https://unity.com/" target="_blank">Unity</a>. </li>
-        <li>NOTE: The game will automatically open in full screen and the quit game button will cause the game to freeze.</li>
-    </ul>
-    </div>
-
-    <div class="div2">
-    <h3>Full Stack Web Application</h3>
-    <img src="img/project_images/mean.jpg" height="60px">
-    <ul>
-        <li>The Full Stack Web App was created with the MEAN (MongoDB, Express, Angular and Node.js) stack.</li> 
-        <li>The web app was created for a final project for Web Technologies course. </li>
-    </ul>
-    </div>
-
-    <div class="div2">
-    <h3>Relational Database Application</h3>
-    <img src="img/project_images/mysql.png" height="60px">
-    <ul>
-        <li>Created a relational database application with PHP that accesses a MySQL database through SQL commands for a Database Management Systems course. </li>
-    </ul>
-    </div>
-
-    <div class="div2">
-    <h3>Mobile Android Application</h3>
-    <img src="img/project_images/android.png" height="60px">
-    <img src="img/project_images/java.png" height="60px">
-    <ul>
-        <li>This application is being created for a Software Engineering Design course. </li>
-        <li>The application was created in Android Studio. </li>
-    </ul>
     </div>
   </div>
 </template>
 
 <script>
+import projectsData from '../data/projects.js'
 export default {
-  name: 'Projects'
+  name: 'Projects',
+  data(){
+    return {
+        projectsJson: projectsData
+    }
+  }
 }
 </script>
 
@@ -66,20 +33,25 @@ export default {
 .div2{
   padding-top: 1em;
   padding-bottom: 1em;
+  padding-right: 0.5em;
   background: var(--container-color); 
   border-style: ridge;
   border-width: 0.1em; 
   border-color: var(--border-color);
   margin: 2px;
   width: 32%;
-  min-width: 350px;
-  min-height: 350px;
+  min-width: 400px;
+  min-height: 400px;
   display: inline-block;
   vertical-align: top; 
 }
 
 .div2 p, .div2 ul, .div2 td, .div2 th {
 color: var(--inactive-p-color);
+}
+
+p, ul {
+  margin-bottom: 5px;
 }
 
 .div2:hover p, .div2:hover ul, .div2:hover td{
@@ -93,6 +65,10 @@ transition: 0.3s;
 
 .div2 img{
   max-width: 300px;
+  display: inline-block;
+  margin-top: 0.5em;
+  margin-bottom: 0.5em;
 }
+
 
 </style>
--- mysite/src/data/projects.js
+++ mysite/src/data/projects.js
@@ -1,5 +1,82 @@
 export default [
     {
-        title: "Personal Website",
-    }
+        title: 'Personal Website',
+        link: 'https://michaeldsilva.com',
+        linkDisplay: 'michaeldsilva.com',
+        repo: 'michaelddsilva.github.io',
+        img: [
+            'img/project_images/m.png', 
+            'img/project_images/vue.png'
+        ],
+        code: 'https://github.com/michaelddsilva/michaelddsilva.github.io',
+        description: [
+            'The repository for the site can be located <a href="https://github.com/michaelddsilva/michaelddsilva.github.io" target ="_blank">here</a>.',
+            'The icons i\'m using in site sections are from <a target="_blank" href="https://icons8.com">Icons8</a>.',
+            'I started this site in Febuary of 2019 and have been updating it with my recent experiences, and tweaking it as I improve my experience as a web developer.',
+            'This site implements <a href="https://vuejs.org/">Vue.js</a>.',
+        ]
+    },
+    {
+        title: 'Shoot \'em up Game',
+        link: 'https://michaelddsilva.github.io/game/',
+        linkDisplay: 'play here',
+        repo: '',
+        img: [
+            'img/project_images/unity.png', 
+            'img/project_images/c_sharp.png'
+        ],
+        code: 'https://github.com/michaelddsilva/michaelddsilva.github.io',
+        description: [
+            'This "shoot \'em up game" was created in collaboration with two other software engineering students.',
+            'The game\'s scripts were coded in C# and the assets and scenes were created in <a href="https://unity.com/" target="_blank">Unity</a>.',
+            'NOTE: The game will automatically open in full screen and the quit game button will cause the game to freeze.',
+        ]
+    },
+    {
+        title: 'Full Stack Web Application',
+        link: '',
+        linkDisplay: 'play here',
+        repo: '',
+        img: [
+            'img/project_images/mongodb.png', 
+            'img/project_images/angular.png',
+            'img/project_images/nodejs.png'
+        ],
+        code: 'https://github.com/michaelddsilva/michaelddsilva.github.io',
+        description: [
+            'The Full Stack Web App was created with the MEAN (MongoDB, Express, Angular and Node.js) stack.',
+            'The web app was created for a final project for Web Technologies course.',
+        ]
+    },
+    {
+        title: 'Relational Database Application',
+        link: '',
+        linkDisplay: '',
+        repo: '',
+        img: [
+            'img/project_images/mysql.png', 
+            'img/project_images/php.png',
+        ],
+        code: 'https://github.com/michaelddsilva/michaelddsilva.github.io',
+        description: [
+            'This application was created for a Database Management Systems course project.',
+            'Created a relational database application with PHP that accesses a MySQL database through SQL commands.',
+        ]
+    },
+    {
+        title: 'Mobile Android Application',
+        link: '',
+        linkDisplay: '',
+        repo: '',
+        img: [
+            'img/project_images/android.png', 
+            'img/project_images/java.png',
+            'img/project_images/firebase.png',
+        ],
+        code: 'https://github.com/michaelddsilva/michaelddsilva.github.io',
+        description: [
+            'This application was created for a Software Engineering Design course.',
+            'The application was created in Android Studio and connects to a <a href="https://firebase.google.com/">Google Firebase</a> database.',
+        ]
+    },
 ]
